c3acd3f7f18079b06ff5b5f074d47753acebf068
Franciane SL <fran.schier@outlook.com>
2025-08-09 20:30:02 -0300

    Implementação das rotas para as telas ListaDeInstrumentosEmTabela e ListaDeInstrumentosEmCards.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import {Route, Routes} from 'react-router-dom';
 import './App.css'
+import ListaDeInstrumentosEmCards from './Pages/ListaDeInstrumentosEmCards';
+import ListaDeInstrumentosEmTabela from './Pages/ListaDeInstrumentosEmTabela';
 
 function App() {
  
   return (
     <>
-      
+    <Routes>
+      <Route path= "/listaemcards" Component={ListaDeInstrumentosEmCards}/>
+      <Route path= "/listaemtabela" Component={ListaDeInstrumentosEmTabela}/>
+    </Routes>  
     </>
-  )
+  );
 }
 
 export default App
