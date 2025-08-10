9805e848bbe4dbaa89ceebb8c18dd638eb7d03b6
Franciane SL <fran.schier@outlook.com>
2025-08-10 20:29:42 -0300

    Implementação do componente EquipamentosLista e o do fetch dos equipamentos via axios.

--- src/Pages/ListaDeInstrumentosEmCards.jsx
+++ src/Pages/ListaDeInstrumentosEmCards.jsx
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import EquipamentosLista from "../Components/Equipamentoslista";
+
 function ListaDeInstrumentosEmCards() {
+  const [equipamentos, setEquipamentos] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:3000/equipamentos")
+    .then((response) => {
+      setEquipamentos(response.data);
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar equipamentos:", error);
+    });
+  }, []);
  
   return (
     <>
-      
+      <article>
+        <h2>Lista de Instrumentos</h2>
+        <section>
+          {equipamentos.map((equipamento) => (
+            <EquipamentosLista
+            key = {equipamento.id}
+            equipamento = {equipamento}
+            
+            />
+
+
+          )
+        )}
+        </section>
+      </article>
     </>
   );
 }
